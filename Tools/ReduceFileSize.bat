538509810b1448456a034036f414ebf85331cf21
CJ Mayes <66642883+CJ-Mayes@users.noreply.github.com>
2022-11-06 18:40:24 +0000

    Add files via upload
    This File can be used to reduce the size of a CSV based on line rows, to be able to upload to GitHub

---
+++ Tools/ReduceFileSize.bat
@@ -0,0 +1,22 @@
+@echo off
+setlocal ENABLEDELAYEDEXPANSION
+REM Edit this value to change the name of the file that needs splitting. Include the extension.
+SET BFN=InsertFileNameHere.csv
+REM Edit this value to change the number of lines per file.
+SET LPF=60000
+REM Edit this value to change the name of each short file. It will be followed by a number indicating where it is in the list.
+SET SFN=InsertFileNameHereeSplit
+REM Do not change beyond this line.
+SET SFX=%BFN:~-3%
+SET /A LineNum=0
+SET /A FileNum=1
+For /F "delims==" %%l in (%BFN%) Do (
+SET /A LineNum+=1
+echo %%l >> %SFN%!FileNum!.%SFX%
+if !LineNum! EQU !LPF! (
+SET /A LineNum=0
+SET /A FileNum+=1
+)
+)
+endlocal
+Pause
